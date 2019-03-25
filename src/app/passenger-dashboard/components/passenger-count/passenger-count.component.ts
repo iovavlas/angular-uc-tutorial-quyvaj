d11362e7ce8c4de2372d26820895e580418907a8
iovavlas <iovavlas@hotmail.com>
2019-03-25 19:31:58 +0100

    6 files modified

--- src/app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
+++ src/app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+// Interfaces 
+import { Passenger } from '../../interfaces/passenger.interface';
 
 @Component({
   /* Component properties */
@@ -7,6 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: [ './passenger-count.component.css' ]    // CSS-file for this Component  
 })
 export class PassengerCountComponent {
+  @Input()   // tells Angular that the property 'items' is an Input-binding from the container
+  items: Passenger[]; 
 
-  constructor() {}
+  constructor() {
+    console.log('Inside PassengerCountComponent constructor...'); 
+  }
+
+  checkedInCount(): number {
+    if (!this.items) return;  // if there are no passengers available... 
+
+    /*          Array        Callback function(param)     return..                */
+    return this.items.filter( (passenger: Passenger) => passenger.checkedIn).length;
+  }
 }
--- src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+// Interfaces 
+import { Passenger } from '../../interfaces/passenger.interface';
 
 @Component({
   /* Component properties */
   selector: 'passenger-detail',                        // HTML-Element  
   templateUrl: './passenger-detail.component.html',    // Content of HTML-Element 
-  styleUrls: [ './passenger-detail.component.css' ]    // CSS-file for this Component  
+  styleUrls: [ './passenger-detail.component.css',     // CSS-files for this Component
+               '../../containers/passenger-dashboard.component.css' ]      
 })
 export class PassengerDetailComponent {
+  @Input()  // tells Angular that the property 'detail' is an Input-binding from the container
+  detail: Passenger; 
 
-  constructor() {}
+  constructor() {
+    console.log('Inside PassengerDetailComponent constructor...'); 
+  }
 }
