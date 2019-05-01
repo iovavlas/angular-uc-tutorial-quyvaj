ba196f91deea4bfd3688d592da5b1ebd1f47b85d
iovavlas <iovavlas@hotmail.com>
2019-05-01 22:55:02 +0200

    2 files modified

--- src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -26,6 +26,8 @@ export class PassengerFormComponent {
     }
   }
 
+  /* If we want to have access to the form, before we actually submit the form, for example if we want to validate sth. before submitting, then we ca do this with @ViewChild, since we already have a #ref variable for the whole form -> '#form' */  
+  /* If we pass the whole form in to this method, instead of "form.value", then this argument is of type NgForm (not Passenger) */ 
   handleSubmit(passenger: Passenger, isValid: boolean) {
     if (isValid) {
       console.log("emitting event inside the 'handleSubmit' method...");
